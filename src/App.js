e8f1a1c2dc53105f93be692b13d2bf13f24b9a8e
minji <eunoia2040@gmail.com>
2022-02-03 15:55:54 +0900

    feat: add sample redux-saga logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,32 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import useSocket from "./hooks/useSocket";
+import { getCatsFetch } from "./state";
+
 function App() {
+  const [socket, disconnect] = useSocket("test");
+  const cats = useSelector((state) => state.cats.cats);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    socket.emit("test", "socket test");
+
+    socket.on("test", (msg) => {
+      console.log(msg);
+    });
+
+    return () => {
+      disconnect();
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    dispatch(getCatsFetch());
+  }, [dispatch]);
+
+  console.log(cats);
+
   return (
     <div className="App">
       <header className="App-header">
