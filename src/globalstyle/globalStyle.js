93a9e489c84547a84f0fa85f8f3e3bf4e841f0ac
minji <eunoia2040@gmail.com>
2022-02-03 22:09:53 +0900

    feat: add global style, theme

---
+++ src/globalstyle/globalStyle.js
@@ -0,0 +1,81 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  html {
+    background-color: #F3F8FB;
+    line-height: 1.2;
+    word-wrap: break-word;
+    font-size:10px;
+    font-family: 'Apple SD Gothic Neo', Roboto, 'Noto Sans KR', NanumGothic, 'Malgun Gothic', sans-serif;
+    color: #555;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    background: #eee;
+    -webkit-font-smoothing: antialiased;
+    @media (max-width: 575.98px) {
+      font-size: 1.2rem;
+    }
+    @media (min-width: 576px) and (max-width: 1199.98px) {
+      font-size: 1.4px;
+    }
+    @media (min-width: 1200px){
+      font-size: 1.6rem;
+    }
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+
+  article, aside, details, figcaption, figure, footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+
+  div, span, article, section, header, footer, aside, p, ul, li, fieldset, legend, label, a, nav, form {
+    box-sizing: border-box;
+  }
+
+  ol, ul, li {
+    list-style: none;
+  }
+
+  table {
+    border-collapse: collapse;
+    border-spacing: 0;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    border: 0;
+  }
+
+  a {
+    display: inline-block;
+  }
+
+  button {
+    border: 0;
+    background: transparent;
+    curs
+  }
+`;
+
+export default GlobalStyle;
---
+++ src/globalstyle/theme.js
@@ -0,0 +1,80 @@
+const calcRem = (size) => `${size}rem`;
+
+const fontSizes = {
+  small: calcRem(1.2),
+  base: calcRem(1.4),
+  lg: calcRem(1.6),
+  xl: calcRem(2.0),
+  xxl: calcRem(2.2),
+  xxxl: calcRem(2.4),
+  titleSize: calcRem(5.0),
+};
+
+const paddings = {
+  small: calcRem(0.4),
+  base: calcRem(0.8),
+  lg: calcRem(1.2),
+  xl: calcRem(1.4),
+  xxl: calcRem(1.6),
+  xxxl: calcRem(1.8),
+};
+
+const margins = {
+  small: calcRem(0.4),
+  base: calcRem(0.8),
+  lg: calcRem(1.2),
+  xl: calcRem(1.4),
+  xxl: calcRem(1.6),
+  xxxl: calcRem(1.8),
+};
+
+const deviceSizes = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "450px",
+  tablet: "768px",
+  tabletL: "1024px",
+};
+
+const colors = {
+  white: "#FFFFFF",
+  black: "#000000",
+  gray_1: "#F0EDED",
+  gray_2: "#C4C4C4",
+  gray_3: "#8C8A8A",
+  green_1: "#5CC793",
+  green_2: "#3cb46e",
+  red: "#D16D6A",
+  purple: "#986DF3",
+};
+
+const opacityColor = {
+  gray: "rgba(230, 230, 233, 0.56)",
+  purple: "rgba(139, 84, 255, 0.4)",
+};
+
+const gradientColor = {
+  lightPurple: "linear-gradient(180deg, #FFC6F5 0%, #E478EB 32.29%, #B577F2",
+  purple: "linear-gradient(180deg, #794AFF 0%, #9A4AFF 70.83%, #B04AFF 96.35%)",
+};
+
+const device = {
+  mobileS: `only screen and (max-width: ${deviceSizes.mobileS})`,
+  mobileM: `only screen and (max-width: ${deviceSizes.mobileM})`,
+  mobileL: `only screen and (max-width: ${deviceSizes.mobileL})`,
+  tablet: `only screen and (max-width: ${deviceSizes.tablet})`,
+  tabletL: `only screen and (max-width: ${deviceSizes.tabletL})`,
+};
+
+const theme = {
+  fontSizes,
+  colors,
+  opacityColor,
+  gradientColor,
+  deviceSizes,
+  device,
+  paddings,
+  margins,
+};
+
+export default theme;
