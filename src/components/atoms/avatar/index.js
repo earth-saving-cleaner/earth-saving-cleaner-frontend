d2af723651b209532d575a2cf102466e80488bf9
JK-Yun777 <ramieta16@gmail.com>
2022-02-04 20:00:57 +0900

    fix: Fix and add getsize function

--- src/components/atoms/avatar/index.js
+++ src/components/atoms/avatar/index.js
@@ -2,29 +2,59 @@ import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
+function getSize(size) {
+  let width = "5rem";
+  let heigth = "5rem";
+
+  switch (size) {
+    case "xs":
+      width = "2.5rem";
+      heigth = "2.5rem";
+      break;
+    case "sm":
+      width = "5rem";
+      heigth = "5rem";
+      break;
+    case "md":
+      width = "10rem";
+      heigth = "10rem";
+      break;
+    case "lg":
+      width = "15rem";
+      heigth = "15rem";
+      break;
+    default:
+      width = `${size}rem`;
+      heigth = `${size}rem`;
+  }
+
+  return { width, heigth };
+}
+
 const StyledImg = styled.img`
   border-radius: 50%;
+  width: ${(props) => getSize(props.size).width};
+  height: ${(props) => getSize(props.size).heigth};
 `;
 
-function Avatar({ url, ...props }) {
-  return <StyledImg {...props} src={url} />;
+function Avatar({ url, size, ...props }) {
+  return <StyledImg {...props} size={size} src={url} />;
 }
 
 Avatar.propTypes = {
   url: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
 };
 
 export default Avatar;
 
-// User Ex:
+// Use Ex:
 // const StyledAvatar = styled(Avatar)`
-//   width: 100px;
-//   height: 100px;
 //   margin: 15px;
-//   background: ${({ theme }) => theme.colors.white};
 // `;
 
 // <StyledAvatar
 //   alt="userImage"
+//   size="sm"
 //   url="https://lh3.googleusercontent.com/a/AATXAJzdJ5gTfflTC1--vXDDRH1n-wX7NQ9mJRViLtgc=s96-c"
-// />
+// />;
--- src/components/atoms/text/index.js
+++ src/components/atoms/text/index.js
@@ -3,25 +3,28 @@ import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const StyledText = styled.div`
+  font-size: ${({ theme, size }) => theme.fontSizes[size]};
   font-weight: ${(props) => props.weight || "normal"};
 `;
 
-function Text({ text, ...props }) {
-  return <StyledText {...props}>{text}</StyledText>;
+function Text({ text, size, ...props }) {
+  return (
+    <StyledText size={size} {...props}>
+      {text}
+    </StyledText>
+  );
 }
 
 Text.propTypes = {
   text: PropTypes.string.isRequired,
+  size: PropTypes.string.isRequired,
 };
 
 export default Text;
 
 // Use Ex:
 // const StyledText = styled(Text)`
-//   flex: none;
-//   margin-right: 50px;
-//   font-weight: bold;
-//   background: ${({ theme }) => theme.colors.red};
+//   margin: 15px;
 // `;
 
-// <StyledText text="ahah" size="2rem" />
+// <StyledText text="textTest" size="titleSize" weight="200" />
