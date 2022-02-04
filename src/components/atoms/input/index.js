2038734afe8326da77dfb738d8ba7c38730811ff
Kyeong1024 <aofldjxm@gmail.com>
2022-02-04 20:09:57 +0900

    fix: Fix button  image input textarea

--- src/components/atoms/input/index.js
+++ src/components/atoms/input/index.js
@@ -1,33 +1,44 @@
 import React from "react";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import PropTypes from "prop-types";
+import themes from "../../../globalstyle/theme";
+
+const { colors, fontSizes } = themes;
+const WRITE_HERE = "write here...";
 
 const StyledInput = styled.input`
-  background: ${(props) => props.background || "white"};
-  color: ${(props) => props.color || "black"};
-  width: ${(props) => props.width || "50%"};
-  height: ${(props) => props.height || "30px"};
-  border-radius: ${(props) => props.radius || "0px"};
-  font-size: ${(props) => props.fontSize || "1.5em"};
-  border-color: ${(props) => props.boderColor || "#cfcfcf"};
-
-  ${(props) =>
-    props.primary &&
-    css`
-      color: black;
-      background: white;
-      border-color: #cfcfcf;
-      border-radius: 5px;
-    `}
+  background: ${(props) => props.background};
+  color: ${(props) => props.color};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font-size: ${(props) => props.fontSize};
+  border-color: ${(props) => props.borderColor};
 `;
-const Input = ({ ...props }) => {
+
+function Input({ ...props }) {
   const { placeholder } = props;
 
   return <StyledInput placeholder={placeholder} {...props} />;
-};
+}
 
 Input.propTypes = {
   placeholder: PropTypes.string,
+  background: PropTypes.string,
+  color: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+  fontSize: PropTypes.string,
+  borderColor: PropTypes.string,
+};
+
+Input.defaultProps = {
+  placeholder: WRITE_HERE,
+  background: colors.white,
+  color: colors.black,
+  width: "20rem",
+  height: "3rem",
+  fontSize: fontSizes.base,
+  borderColor: colors.purple,
 };
 
 export default Input;
--- src/components/atoms/textarea/index.js
+++ src/components/atoms/textarea/index.js
@@ -1,18 +1,37 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
+import themes from "../../../globalstyle/theme";
+
+const { fontSizes, paddings } = themes;
 
 const StyledTextarea = styled.textarea`
-  width: ${(props) => props.width || "80%"};
-  height: ${(props) => props.width || "7rem"};
-  font-size: ${(props) => props.fontSize || "1.5rem"};
-  border-radius: ${(props) => props.raduis || "0.5rem"};
-  padding: ${(props) => props.padding || "0.5rem"};
-  border-width: ${(props) => props.borderWidth || "0.3rem"};
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  font-size: ${(props) => props.fontSize};
+  padding: ${(props) => props.padding};
+  border-width: ${(props) => props.borderWidth};
   resize: none;
 `;
 
-const Textarea = ({ ...props }) => {
+function Textarea({ ...props }) {
   return <StyledTextarea {...props} />;
+}
+
+Textarea.propTypes = {
+  width: PropTypes.string,
+  heigth: PropTypes.string,
+  fontSize: PropTypes.string,
+  padding: PropTypes.string,
+  borderWidth: PropTypes.string,
+};
+
+Textarea.defaultProps = {
+  width: "300px",
+  heigth: "500px",
+  fontSize: fontSizes.base,
+  padding: paddings.xl,
+  borderWidth: "0.5rem",
 };
 
 export default Textarea;
