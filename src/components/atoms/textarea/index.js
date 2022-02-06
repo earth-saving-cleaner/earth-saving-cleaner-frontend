889656a8b566abbcd9886cfa70745128efd8f334
Kyeong1024 <aofldjxm@gmail.com>
2022-02-06 12:20:25 +0900

    Fix: fix root and import export

--- src/components/atoms/textarea/index.js
+++ src/components/atoms/textarea/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
-import themes from "../../../globalstyle/theme";
+import themes from "../../../theme/theme";
 
 const { fontSizes, paddings } = themes;
 
