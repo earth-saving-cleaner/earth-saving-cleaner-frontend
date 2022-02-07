35c08bafb6482133ae29170652111cf43f623840
minji <eunoia2040@gmail.com>
2022-02-07 22:11:31 +0900

    feat: add MainTemplate component(template)

---
+++ src/components/templates/MainTemplate/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+import { Header } from "../../organisms";
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const ContentWrapper = styled.div`
+  position: absolute;
+  top: 5rem;
+  z-index: 2;
+  overflow: scroll;
+`;
+
+function MainTemplate({ children, ...props }) {
+  return (
+    <StyledContainer>
+      <Header />
+      <ContentWrapper>{children}</ContentWrapper>
+    </StyledContainer>
+  );
+}
+
+MainTemplate.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node]).isRequired,
+};
+
+export default MainTemplate;
