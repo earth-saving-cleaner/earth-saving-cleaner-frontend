3873f54b36ffbfdc032909391f36d881d77df030
minji <eunoia2040@gmail.com>
2022-02-14 22:18:52 +0900

    feat: add socket to MapPage

--- src/components/pages/MapPage/index.js
+++ src/components/pages/MapPage/index.js
@@ -1,22 +1,34 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import GoogleMapReact from "google-map-react";
 import styled from "styled-components";
 
 import { getFeedInfo } from "../../../api";
 import theme from "../../../theme/theme";
-import { Icon } from "../../atoms";
+import { Icon, GpsIcon } from "../../atoms";
 import { MapTemplate } from "../../templates";
+
+import useSocket from "../../../hooks/useSocket";
 
 const StyledIcon = styled(Icon)`
   color: ${(props) => props.color};
 `;
 
 function MapPage() {
+  function getRandom() {
+    const rand09 = Math.floor(Math.random() * 10);
+    return rand09;
+  }
+
+  const random = `0.000${getRandom()}`;
+  const [socket, disconnect] = useSocket("map");
   const [defaultProps, setDefaultProps] = useState(null);
   const [zoomLevel, setZoomLevel] = useState(14);
   const [boundary, setBoundary] = useState({});
   const [feedLocation, setFeedLocation] = useState([]);
+  // const userGps = useRef();
+  const [userGps, setUserGps] = useState();
+  const [gpsLocations, setgpsLocations] = useState({});
 
   function spreadFeeds() {
     return feedLocation.map((feed) => {
@@ -24,15 +36,49 @@ function MapPage() {
       const [longitude, latitude] = coordinates;
       const iconType = cleaned ? "leaf" : "trashCanFill";
       const color = cleaned ? theme.colors.green_1 : theme.colors.red;
-
-      return <StyledIcon key={_id} lat={latitude} lng={longitude} icon={iconType} color={color} />;
+      return <StyledIcon key={_id} lat={latitude} lng={longitude} icon={iconType} color={color} size="sm" />;
     });
   }
+
+  function paintGpsLocations() {
+    return Object.keys(gpsLocations)?.map((socketId) => {
+      const { location, sid } = gpsLocations[socketId];
+
+      if (location?.longitude) {
+        if (socket.id === sid) {
+          return <GpsIcon key={sid} color="yellow" lat={location.latitude} lng={location.longitude} />;
+        }
+
+        return <GpsIcon key={sid} color="purple" lat={location.latitude} lng={location.longitude} />;
+      }
+
+      return null;
+    });
+  }
+
+  function watchPositionSuccess(position) {
+    const { longitude, latitude } = position.coords;
+    setUserGps({ longitude, latitude });
+  }
+
+  function watchPositionError(err) {
+    console.warn(err.code, err.message);
+  }
+
+  const options = {
+    enableHighAccuracy: false,
+    timeout: 20000,
+    maximumAge: Infinity,
+  };
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
       (position) => {
         const { latitude, longitude } = position.coords;
+
+        socket.emit("join", { userId: "testId", location: { longitude, latitude } });
+
+        setUserGps({ longitude, latitude });
 
         setDefaultProps({
           // user accept current location.
@@ -47,13 +93,20 @@ function MapPage() {
         setDefaultProps({
           // user denied user current location.
           center: {
-            lat: 37.50805,
+            lat: 37.50815,
             lng: 127.06135,
           },
           zoom: 14,
         });
       },
+      options,
     );
+
+    const watchId = navigator.geolocation.watchPosition(watchPositionSuccess, watchPositionError, options);
+
+    return () => {
+      navigator.geolocation.clearWatch(watchId);
+    };
   }, []);
 
   useEffect(() => {
@@ -66,6 +119,26 @@ function MapPage() {
       getFeedLocation();
     }
   }, [zoomLevel, boundary]);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id);
+    });
+
+    socket.on("locations", (locations) => {
+      setgpsLocations(locations);
+    });
+
+    return () => {
+      disconnect();
+    };
+  }, [socket]);
+
+  useEffect(() => {
+    if (userGps?.longitude) {
+      socket.emit("gps", userGps);
+    }
+  }, [userGps]);
 
   return (
     <MapTemplate>
@@ -85,6 +158,7 @@ function MapPage() {
           }}
         >
           {spreadFeeds()}
+          {paintGpsLocations()}
         </GoogleMapReact>
       )}
     </MapTemplate>
