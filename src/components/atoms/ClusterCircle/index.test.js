2a940c35d6a8fc739c5ec3a440703d3e190ac85b
Kyeong1024 <aofldjxm@gmail.com>
2022-02-19 19:55:58 +0900

    test: add atoms clusterCircle

--- src/components/atoms/ClusterCircle/index.test.js
+++ src/components/atoms/ClusterCircle/index.test.js
@@ -1,11 +1,14 @@
-// import React from "react";
-// import { getByTestId, render, screen } from "@testing-library/react";
+import React from "react";
+import { getByTestId, render, screen } from "@testing-library/react";
 
-// import ClusterCircle from ".";
+import ClusterCircle from ".";
 
-// describe("<ClusterCircle/>", () => {
-//   it("has width", () => {
-//     render(<ClusterCircle data-testid="cluster" />);
-//     const id = getByTestId(result);
-//   });
-// });
+describe("<ClusterCircle/>", () => {
+  it("has width", () => {
+    render(<ClusterCircle data-testid="cluster" size={3} />);
+    const cluster = screen.getByTestId("cluster");
+
+    expect(getComputedStyle(cluster).width).toBe("9rem");
+    expect(cluster).toBeInTheDocument();
+  });
+});
